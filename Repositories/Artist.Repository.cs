6abdcf10c45405a11995b5cc597b58448530613a
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-11-26 11:17:53 -0400

    feat: including repositories for Artist, creating conection with ArtistDetail inside GetAllAsync method

---
+++ Repositories/Artist.Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniSpotify.Data;
+using MiniSpotify.Models;
+
+namespace MiniSpotify.Repositories
+{
+    public class ArtistRepository : IArtistRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ArtistRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Artist>> GetAllAsync()
+        {
+            return await _context.Artists
+                .Include(a => a.ArtistDetail)
+                .ToListAsync();
+        }
+
+        public async Task<Artist?> GetByIdAsync(Guid id)
+        {
+            return await _context.Artists
+                .Include(a => a.ArtistDetail)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task AddAsync(Artist artist)
+        {
+            await _context.Artists.AddAsync(artist);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Artist artist)
+        {
+            _context.Artists.Update(artist);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Artist artist)
+        {
+            _context.Artists.Remove(artist);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
