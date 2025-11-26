618f2f23091c7fb4517dd81c391da134ccd3b007
Dabner Orozco <dabner.orozco@ucb.edu.bo>
2025-11-26 19:37:00 -0400

    feat: Creating Controller for Artist

---
+++ Controllers/Artist.Controller.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniSpotify.Models;
+using MiniSpotify.Models.DTOS;
+using MiniSpotify.Services;
+
+namespace MiniSpotify.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtistController : ControllerBase
+    {
+        private readonly IArtistService _service;
+
+        public ArtistController(IArtistService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Artist>>> GetAll()
+        {
+            return Ok(await _service.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Artist>> GetById(Guid id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Artist>> Create(CreateArtistDto dto)
+        {
+            var result = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, UpdateArtistDto dto)
+        {
+            var result = await _service.UpdateAsync(id, dto);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _service.DeleteAsync(id);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+    }
+}
